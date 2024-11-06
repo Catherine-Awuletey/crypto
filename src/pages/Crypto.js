94d7f43ba57960e181472dce16d25f7d8c4a8411
Cattie <87996502+Catherine-Awuletey@users.noreply.github.com>
2024-11-06 16:45:18 +0000

    Initial commit

---
+++ src/pages/Crypto.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import TableComponents from '../components/TableComponents'
+
+const Crypto = () => {
+  return (
+    <section className='w-[80%] h-full flex flex-col mt-16 mb-24 relative'
+    
+    >
+    <TableComponents/>
+    </section>
+  )
+}
+
+export default Crypto
